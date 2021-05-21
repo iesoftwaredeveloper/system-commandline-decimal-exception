f809887298d7c8a7e4616fb227ae8b03f7ea42e6
Robert Little <rll04747@pomona.edu>
2021-05-21 14:02:31 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
+using System.CommandLine.Parsing;
+
+namespace system_commandline_decimal_exception
+{
+    /// <summary>
+    /// Successful example
+    /// 
+    /// $ dotnet run -- --opt-decimal 123.456
+    /// ParseResult: [ system-commandline-decimal-exception [ --opt-decimal <123.456> ] ]
+    /// --opt-decimal 123.456
+    /// 
+    /// Exception
+    /// 
+    /// $ dotnet run -- 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand()
+            {
+                Description = "Console app to demonstrate System.CommandLine complex type exception"
+            };
+            rootCommand.Add(new Option<decimal>(new[] { "--opt-decimal" }, description: "A decimal option"));
+            rootCommand.Handler = CommandHandler.Create((ParseResult parseResult, IConsole console, ComplexType options) =>
+               {
+                   console.Out.WriteLine($"{parseResult}");
+                   console.Out.WriteLine($"--opt-decimal {options.OptDecimal}");
+               });
+
+            rootCommand.InvokeAsync(args);
+        }
+    }
+
+    public class ComplexType
+    {
+        public decimal OptDecimal { get; set; }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# system-commandline-decimal-exception
+
+This is an example project to demonstrate how an exception occurs when you try to use a ComplexType that includes a decimal value.
+
+The exception only appears to occur when you do not provide the exception on the command line.
